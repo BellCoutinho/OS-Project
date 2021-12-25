93b1bbcbe92b51f9786d152421f4d37f105ef10b
BellCoutinho <bell.sccoutinho@gmail.com>
2021-12-25 18:24:30 -0300

    ✨ - feat(Scheduling): Add methods to the Scheduler interface

--- src/scheduling/Scheduler.java
+++ src/scheduling/Scheduler.java
@@ -1,7 +1,13 @@
 package scheduling;
 
+import java.util.Queue;
+
 import entity.ProcessControlBlock;
 
 public interface Scheduler {
-    public void execute(ProcessControlBlock pcb);
+    public void admitted(Queue<ProcessControlBlock> readyQueue, Runnable code);
+    public void dispatch(ProcessControlBlock pcb);
+    public void interrupt(ProcessControlBlock pcb);
+    public void exit(ProcessControlBlock pcb);
+    //public void execute(ProcessControlBlock pcb);
 }
